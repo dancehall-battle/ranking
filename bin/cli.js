11b315b60b6d7885d3b0684bfeeca16df1e13d4e
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-07-26 09:40:03 +0200

    add ranking per country

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const rank = require('../lib/dancers');
+const rankDancers = require('../lib/dancers');
+const rankCountries = require('../lib/countries');
 
 program
 //  .version('0.1.0')
   .option('-p, --participants [number]', 'Select battle with a given number of participants (default: 1,2).', parseParticipants)
+  .option('-r, --ranking [rank]', 'Select for what to receive the ranking: dancer or country (default: dancer).', parseRanking)
   .parse(process.argv);
 
 
 program.participants = program.participants || ['1', '2'];
+program.ranking = program.ranking || 'dancer';
 
-rank(program.participants);
+if (program.ranking === 'dancer') {
+  rankDancers(program.participants);
+} else {
+  rankCountries(program.participants);
+}
 
 function parseParticipants(participants) {
   if (participants) {
@@ -25,6 +32,19 @@ function parseParticipants(participants) {
     });
 
     return participants;
+  } else {
+    return null;
+  }
+}
+
+function parseRanking(ranking) {
+  if (ranking) {
+    if (ranking === 'dancer' || ranking === 'country') {
+      return ranking;
+    } else {
+      console.error(`Ranking has to be either "dancer" or "country".`);
+      process.exit(1);
+    }
   } else {
     return null;
   }
