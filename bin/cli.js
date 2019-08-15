878717364a86208d87e6fe1c123fb518d5f1d4da
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-08-15 09:49:17 +0200

    put points in separate json file

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,7 @@ const path = require('path');
 const rankDancers = require('../lib/dancers');
 const rankCountries = require('../lib/countries');
 const pkg = require(path.resolve(__dirname, '../package.json'));
+const {format} = require('date-fns');
 
 const today = new Date();
 const oneYearAgo = new Date();
@@ -12,11 +13,22 @@ oneYearAgo.setFullYear(today.getFullYear() - 1);
 
 program
   .version(pkg.version)
-  .option('-p, --participants [number]', 'Select battle with a given number of participants (default: 1,2).', parseParticipants, ['1','2'])
-  .option('-r, --ranking [rank]', 'Select for what to receive the ranking: dancer or country (default: dancer).', parseRanking, 'dancer')
-  .option('-s, --start-date [date]', 'Start date of the range of battles to consider.', parseDate, oneYearAgo)
+  .option('-p, --participants [number]', 'Select battle with a given number of participants (comma-separated).', parseParticipants, '1,2')
+  .option('-r, --ranking [rank]', 'Select for what to receive the ranking: dancer or country.', parseRanking, 'dancer')
+  .option('-s, --start-date [date]', `Start date of the range of battles to consider.`, parseDate, oneYearAgo)
   .option('-t, --end-date [date]', 'End date of the range of battles to consider.', parseDate, today)
+  .option('-v, --verbose', 'Make the tool more talkative.')
   .parse(process.argv);
+
+program.participants = program.participants.split(',');
+
+if (program.verbose) {
+  console.log('Ranking: ' + program.ranking);
+  console.log('Participants: ' + program.participants);
+  console.log('Start date: ' + format(program.startDate, 'yyyy-MM-dd'));
+  console.log('End date: ' + format(program.endDate, 'yyyy-MM-dd'));
+  console.log();
+}
 
 if (program.ranking === 'dancer') {
   rankDancers(program.participants, program.startDate, program.endDate);
@@ -26,9 +38,9 @@ if (program.ranking === 'dancer') {
 
 function parseParticipants(participants) {
   if (participants) {
-    participants = participants.split(',');
+    const temp = participants.split(',');
 
-    participants.forEach(p => {
+    temp.forEach(p => {
       if (isNaN(parseInt(p))) {
         console.error(`${p} is not a number.`);
         process.exit(1);
