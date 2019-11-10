945107671ef14718873de1c7939a327ed7bef225
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-11-10 17:49:21 +0100

    update script for updating ldf config

--- scripts/update-ldf-config.js
+++ scripts/update-ldf-config.js
@@ -9,16 +9,17 @@ program
   .requiredOption('-c, --config <path>', 'Path to config file of ldf-server.')
   .requiredOption('-f, --file <path>', 'Path to JSON-LD file.')
   .requiredOption('-d, --datasource <string>', 'Name of data source in config file.')
+  .requiredOption('-t, --title <string>', 'Name of data source in config file.')
   .parse(process.argv);
 
 const ldfConfig = fs.readJsonSync(path.resolve(process.cwd(), program.config));
-const today = '' + format(new Date(), 'yyyy-MM-dd');
-ldfConfig.datasources[today] = {
-  "type": "JsonLdDatasource",
-  "settings": { "file": program.file }
+ldfConfig.datasources[program.title] = {
+  type: "JsonLdDatasource",
+  hide: true,
+  settings: { "file": program.file }
 };
 
 const datasource = ldfConfig.datasources[program.datasource];
-datasource.settings.references.push(today);
+datasource.settings.references.push(program.title);
 
-console.dir(ldfConfig, {depth: null});
+console.log(JSON.stringify(ldfConfig));
